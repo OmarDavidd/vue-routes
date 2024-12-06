a1672c247e1ec64a825f359a960a9ad916803280
Davo <132635516+OmarDavidd@users.noreply.github.com>
2024-12-06 11:09:11 -0600

    Update HomePage.vue

--- src/HomePage.vue
+++ src/HomePage.vue
@@ -8,19 +8,16 @@
 			<h3 class="text-3xl font-extrabold text-gray-800 mb-10 text-center">Welcome to my home page</h3>
 			<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6">
 				<div class="bg-gray-200 rounded-lg shadow-md p-6">
-					<p class="text-center mb-4">Descubre los últimos lanzamientos, reseñas y consejos para mejorar tu experiencia
-						de juego.</p>
+					<p class="text-center mb-4">Juegos shoters populares</p>
 				</div>
 				<div class="bg-gray-200 rounded-lg shadow-md p-6">
-					<p class="text-center mb-4">Mantente actualizado con reseñas, tráilers y recomendaciones de los últimos éxitos
-						de taquilla.</p>
+					<p class="text-center mb-4">Peliculas populares</p>
 				</div>
 				<div class="bg-gray-200 rounded-lg shadow-md p-6">
-					<p class="text-center mb-4">Sumérgete en artículos exclusivos, listas y opiniones sobre el mundo del
-						entretenimiento.</p>
+					<p class="text-center mb-4">Posts</p>
 				</div>
 				<div class="bg-gray-200 rounded-lg shadow-md p-6">
-					<p class="text-center mb-4">Explora reseñas y guías para tus series y programas favoritos.</p>
+					<p class="text-center mb-4">Tv shows populares</p>
 				</div>
 			</div>
 		</div>
